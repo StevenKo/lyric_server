4d81b404e7a8b36715e660343983bc3174d27bf0
Steven Ko <chunyuko85@gmaill.com>
2013-04-07 21:09:19 +0800

    album

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -2,23 +2,23 @@ class Api::V1::AlbumsController < Api::ApiController
 
   def index
     singer_id = params[:singer_id]
-    albums = Album.where("singer_id = #{singer_id}")
+    albums = Album.where("singer_id = #{singer_id}").select_id_name_release
     render :json => albums
   end
 
   def show
-    album = Album.find(params[:id])
+    album = Album.select("id,name,release_time,description").find(params[:id])
     render :json => album
   end
 
   def new_albums
-    albums = Album.where('is_new = true')
+    albums = Album.where('is_new = true').select_id_name_release.paginate(:page => params[:page], :per_page => 15)
     render :json => albums
   end
 
   def hot_albums
     category_id = params[:category_id]
-    albums = Album.where("hot_album_category_id = #{category_id}")
+    albums = Album.where("hot_album_category_id = #{category_id}").select_id_name_release
     render :json => albums
   end
 
--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,5 @@ class Album < ActiveRecord::Base
   belongs_to :singer
   belongs_to :hot_album_category
   has_many :songs
+  scope :select_id_name_release, select("id,name,release_time")
 end
