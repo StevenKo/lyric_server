d0deaadb7b72eefe763276d9285101b96277028c
Steven Ko <chunyuko85@gmaill.com>
2013-04-07 18:19:00 +0800

    search way item

--- app/controllers/api/v1/singer_search_way_items_controller.rb
+++ app/controllers/api/v1/singer_search_way_items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SingerSearchWayItemsController < Api::ApiController
   def index
-    items = SingerSearchWayItem.where("singer_search_way_id = #{params[:singer_search_way_id]}")
+    items = SingerSearchWayItem.select("id,name,singer_search_way_id")
     render :json => items
   end
 end
