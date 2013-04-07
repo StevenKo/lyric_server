005ec48e28ce9627f101241f3ae1827fbe1e4cda
Steven Ko <chunyuko85@gmaill.com>
2013-04-07 22:17:58 +0800

    pagenate and song show

--- app/controllers/api/v1/singers_controller.rb
+++ app/controllers/api/v1/singers_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::SingersController < Api::ApiController
     # Singer.includes(:singer_search_way_item_relations).where("singer_search_way_item_relations.singer_search_way_item_id = 1")
     
     ids = SingerSearchWayItemRelation.where("singer_search_way_item_id = #{params[:serch_item_id]}").select("singer_id").map{|r| r.singer_id }
-    singers = Singer.select("id, name").where("id in (#{ids.join(",")})")
+    singers = Singer.select("id, name").where("id in (#{ids.join(",")})").paginate(:page => params[:page], :per_page => 30)
     render :json => singers
   end
 
@@ -16,7 +16,7 @@ class Api::V1::SingersController < Api::ApiController
   end
 
   def hot_singers
-    singers = Singer.select("id, name").where("singer_category_id = #{params[:singer_category_id]} and is_hot = true")
+    singers = Singer.select("id, name").where("singer_category_id = #{params[:singer_category_id]} and is_hot = true").paginate(:page => params[:page], :per_page => 30)
     render :json => singers
   end
 end
--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -2,13 +2,18 @@ class Api::V1::SongsController < Api::ApiController
   
   def index
     singer_id = params[:singer_id]
-    songs = Song.where("singer_id = #{singer_id}").paginate(:page => params[:page], :per_page => 15)
+    songs = Song.where("singer_id = #{singer_id}").select("id,name,album_id").paginate(:page => params[:page], :per_page => 30)
     render :json => songs
+  end
+
+  def show
+     song = Song.select("id,name,album_id,lyric").find(params[:id])
+     render :json => song
   end
 
   def hot_songs
     category_id = params[:category_id]
-    songs = Song.where("hot_song_category_id = #{category_id}").select("id,name,album_id")
+    songs = Song.where("hot_song_category_id = #{category_id}").select("id,name,album_id").paginate(:page => params[:page], :per_page => 30)
     render :json => songs
   end
 
