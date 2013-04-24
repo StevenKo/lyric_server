e78f3c121425cef8103163efbb658ef0d062b01e
Steven Ko <chunyuko85@gmaill.com>
2013-04-24 21:29:07 +0800

    singer order is_hot

--- app/models/singer.rb
+++ app/models/singer.rb
@@ -12,11 +12,12 @@ class Singer < ActiveRecord::Base
   def self.search(params)
     tire.search(page: params[:page], per_page: 24, load: true) do
       query { string params[:keyword], default_operator: "AND" }
+      sort{by :is_hot, 'desc'}
     end
   end
 
   mapping do
-    indexes :id, type: 'integer'
+    indexes :is_hot, type: 'boolean'
     indexes :name, :analyzer => "cjk"
   end
 
