05e43a36d89c9e4146edba25c17f24e00547109f
Steven Ko <chunyuko85@gmaill.com>
2013-05-31 17:56:19 +0800

    shuffle the videos

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::VideosController < Api::ApiController
   def index
     videos = Video.select("title, youtube_id")
-    render :json => videos
+    render :json => videos.shuffle
   end
 end
