34a93bcdb02735f723c77533adb3f7be772e841e
Steven Ko <chunyuko85@gmaill.com>
2013-07-31 15:58:06 +0800

    add user update info

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,4 +20,82 @@ class Api::V1::UsersController < ApplicationController
     render :status=>200, :json => {"message" => "success"}
   end
 
+  def update_looked_songs
+    regid = params[:regid]
+    song = params[:song]
+    user = User.find_by_registration_id(regid)
+    if user
+      user.looked_songs << song unless user.looked_songs.include? song
+      user.save
+      render :status=>200, :json => {"message" => "success"}
+    else
+      render :status=>404, :json => {"message" => "fail"}
+    end
+  end
+
+  def update_looked_singers
+    regid = params[:regid]
+    singer = params[:singer]
+    user = User.find_by_registration_id(regid)
+    if user
+      user.looked_singers << singer unless user.looked_singers.include? singer
+      user.save
+      render :status=>200, :json => {"message" => "success"}
+    else
+      render :status=>404, :json => {"message" => "fail"}
+    end
+  end
+
+  def update_looked_albums
+    regid = params[:regid]
+    album = params[:album]
+    user = User.find_by_registration_id(regid)
+    if user
+      user.looked_albums << album unless user.looked_albums.include? album
+      user.save
+      render :status=>200, :json => {"message" => "success"}
+    else
+      render :status=>404, :json => {"message" => "fail"}
+    end
+  end
+
+  def update_collected_songs
+    regid = params[:regid]
+    songs = params[:songs]
+    user = User.find_by_registration_id(regid)
+    if user
+      user.collected_songs = songs
+      user.save
+      render :status=>200, :json => {"message" => "success"}
+    else
+      render :status=>404, :json => {"message" => "fail"}
+    end
+  end
+
+  def update_collected_albums
+    regid = params[:regid]
+    albums = params[:albums]
+    user = User.find_by_registration_id(regid)
+    if user
+      user.collected_albums = albums
+      user.save
+      render :status=>200, :json => {"message" => "success"}
+    else
+      render :status=>404, :json => {"message" => "fail"}
+    end
+  end
+
+  def update_collected_singers
+    regid = params[:regid]
+    singers = params[:singers]
+    user = User.find_by_registration_id(regid)
+    if user
+      user.collected_singers = singers
+      user.save
+      render :status=>200, :json => {"message" => "success"}
+    else
+      render :status=>404, :json => {"message" => "fail"}
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,4 @@ class User < ActiveRecord::Base
   serialize :looked_songs
   serialize :looked_singers
   serialize :looked_albums
-  serialize :collected_songs
-  serialize :collected_albums
-  serialize :collected_singers
 end
