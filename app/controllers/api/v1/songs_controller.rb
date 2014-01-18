6799d4f4de32bc77ffa238d52697676b2512a478
Steven Ko <chunyuko85@gmail.com>
2014-01-18 22:10:20 +0800

    top list song]

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -51,7 +51,11 @@ class Api::V1::SongsController < Api::ApiController
 
   def top_list_songs
     list_id = params[:list_id]
-    @songs = Song.joins(:top_lists).includes(:singer).where("top_lists.id = #{list_id}").select("songs.id,songs.name,songs.album_id,songs.singer_id").order("song_top_list_relations.id DESC")
+    if list_id == "31"
+      @songs = Song.joins(:top_lists).includes(:singer).where("top_lists.id = #{list_id}").select("songs.id,songs.name,songs.album_id,songs.singer_id").order("song_top_list_relations.id AES")
+    else
+      @songs = Song.joins(:top_lists).includes(:singer).where("top_lists.id = #{list_id}").select("songs.id,songs.name,songs.album_id,songs.singer_id").order("song_top_list_relations.id DESC")
+    end
   end
 
   def recommend_songs
